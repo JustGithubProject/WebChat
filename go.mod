661d264203dd3474c341d2ad5dc98fa8bd843797
JustGithubProject <Kropivadanil@gmail.com>
2024-11-19 19:36:04 +0200

    Simplified websocket server (started to use gorilla/websocket)

---
+++ go.mod
@@ -0,0 +1,5 @@
+module JustGithubProject/WebSocketChat
+
+go 1.21.6
+
+require github.com/gorilla/websocket v1.5.3 // indirect
---
+++ server.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	
+	"github.com/gorilla/websocket"
+)
+
+
+/*
+	Upgrader will be used to upgrade
+	HTTP connections to WebSocket connections
+
+	CheckOrigin allows websocket connections
+	from any origin 
+*/
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+
+type Message struct {
+	Username string `json:"username"`
+	Text string `json:"message"`
+}
+
+
+/*
+	clients - It keeps track of connected WebSocket clients
+	broadcast - It's used to broadcast messages to all connected clients
+*/
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+
+
+func handleHomePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to Home Page")
+}
+
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Failed to upgrade HTTP server connection to websocket protocol")
+		return
+	}
+	defer conn.Close()
+
+	clients[conn] = true
+
+	for {
+		var msg Message
+		err := conn.ReadJSON(&msg)
+		if err != nil {
+			log.Println("Failed to read json")
+			delete(clients, conn)
+			return 
+		}
+		// Writing msg to channel
+		broadcast <- msg
+	}
+}
+
+
+func handleMessages() {
+	for {
+		msg := <-broadcast
+
+		for client := range clients {
+			err := client.WriteJSON(&msg)
+			if err != nil {
+				log.Println("Failed to write json")
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
+
+
+func main() {
+	http.HandleFunc("/", handleHomePage)
+	http.HandleFunc("/ws", handleConnections)
+
+	go handleMessages()
+	
+	http.ListenAndServe(":8000", nil)
+}
